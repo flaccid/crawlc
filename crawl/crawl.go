6f8c7eddb324fd5af1d871fcafe46b000135a468
Chris Fordham <chris@fordham-nagy.id.au>
2019-05-06 15:48:35 +1000

    Support for ctrl+c, some basic results summary, change where responses are logged (still needs some work).

--- crawl/crawl.go
+++ crawl/crawl.go
@@ -2,7 +2,10 @@ package crawl
 
 import (
 	"net/http"
+	"os"
+	"os/signal"
 	"runtime"
+	"strconv"
 	"time"
 
 	"github.com/PuerkitoBio/gocrawl"
@@ -14,14 +17,34 @@ type Ext struct {
 	*gocrawl.DefaultExtender
 }
 
-func (e *Ext) Visit(ctx *gocrawl.URLContext, res *http.Response, doc *goquery.Document) (interface{}, bool) {
-	log.Infof("Visit: %s\n", ctx.URL())
+type ResultSummary struct {
+	Http200 int
+	Http404 int
+}
 
+var (
+	start time.Time
+	responses []*http.Response
+	results ResultSummary
+)
+
+func (e *Ext) Visit(ctx *gocrawl.URLContext, res *http.Response, doc *goquery.Document) (interface{}, bool) {
 	var stats runtime.MemStats
 	runtime.ReadMemStats(&stats)
-	log.Debugf("HeapAlloc=%02fMB; Sys=%02fMB\n", float64(stats.HeapAlloc)/1024.0/1024.0, float64(stats.Sys)/1024.0/1024.0)
+	responses = append(responses, res)
+
+	if log.GetLevel() == log.DebugLevel {
+		log.WithFields(log.Fields{
+			"HeapAlloc": float64(stats.HeapAlloc)/1024.0/1024.0,
+			"Sys": float64(stats.Sys)/1024.0/1024.0,
+		}).Debug("memory")
+	}
 
 	return nil, true
+}
+
+func (e *Ext) End(err error) {
+	report(responses)
 }
 
 func (e *Ext) Filter(ctx *gocrawl.URLContext, isVisited bool) bool {
@@ -31,7 +54,54 @@ func (e *Ext) Filter(ctx *gocrawl.URLContext, isVisited bool) bool {
 	return true
 }
 
+func (e *Ext) ComputeDelay(host string, di *gocrawl.DelayInfo, lastFetch *gocrawl.FetchInfo) time.Duration {
+	if lastFetch != nil {
+		log.WithFields(log.Fields{
+			"url": lastFetch.Ctx.URL(),
+			"host": host,
+			"duration": lastFetch.Duration,
+			"delay-info": di,
+			"status": lastFetch.StatusCode,
+			"head-requst": strconv.FormatBool(lastFetch.IsHeadRequest),
+		}).Info("hit")
+	}
+
+	return di.OptsDelay
+}
+
+func report(r []*http.Response) {
+	elapsed := time.Since(start)
+
+	for _, res := range r {
+		switch res.StatusCode {
+    case 200:
+        results.Http200++
+    case 404:
+        results.Http404++
+    }
+	}
+
+	log.WithFields(log.Fields{
+		"elapsed": elapsed,
+		"total responses": len(r),
+		"http-200": results.Http200,
+		"http-404": results.Http404,
+	}).Info("results")
+}
+
 func Crawl(url string, external bool, delay int, maxVisits int) {
+	start = time.Now()
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	go func(){
+	    for sig := range c {
+					log.Info(sig)
+					report(responses)
+					os.Exit(1)
+	    }
+	}()
+
 	ext := &Ext{&gocrawl.DefaultExtender{}}
 	opts := gocrawl.NewOptions(ext)
 
@@ -44,12 +114,11 @@ func Crawl(url string, external bool, delay int, maxVisits int) {
 	opts.CrawlDelay = time.Duration(delay) * time.Second
 	opts.LogFlags = gocrawl.LogError
 	opts.MaxVisits = maxVisits
-
 	log.Debug(opts, url)
 
 	log.Print("starting crawl...")
-	c := gocrawl.NewCrawlerWithOptions(opts)
-	if err := c.Run(url); err != nil {
+	crawler := gocrawl.NewCrawlerWithOptions(opts)
+	if err := crawler.Run(url); err != nil {
 		log.Print(err)
 	}
 }
